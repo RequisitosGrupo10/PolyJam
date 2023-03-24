908471ee4cb7fbacd8f36113ab830816c17e4a6b
Illya Rozumovskyy <75805551+illyaro@users.noreply.github.com>
2023-03-24 21:32:24 +0100

    Search implemented

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,26 @@ const searchBar = document.getElementById("searchBar")
 const searchButton = document.getElementById("searchButton")
 const searchInput = document.getElementById("searchInput")
 
+function doSearch() {
+    const searchValue = searchInput.value; // if searchValue is empty, show all games
+    const games = gameCatalog.children;
+    for (let i = 0; i < games.length; i++) {
+        const game = games[i];
+        const gameName = game.querySelector('.card-body h5.card-title').textContent;
+        if (gameName.toLowerCase().includes(searchValue.toLowerCase()) || searchValue === "") {
+            game.removeAttribute("style");
+        } else {
+            game.setAttribute("style", "display: none !important;");
+        }
+    }
+}
+
 searchInput.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
-        console.log("Enter pressed")
+        doSearch();
     }
 }, false);
 
 searchButton.addEventListener("click", function (e) {
-    console.log("Search button pressed")
+    doSearch();
 }, false);
