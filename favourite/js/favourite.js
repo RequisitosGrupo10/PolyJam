c3b4eaf088c02a9ba2f1869a03871412da8fc0b5
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-04-12 10:30:46 +0200

    Script initialized

---
+++ favourite/js/favourite.js
@@ -0,0 +1,7 @@
+const matchPairsStar = document.getElementsById("matchPairStar");
+const quickQuizStar = document.getElementsById("quickQuizStar");
+const minesweeperStar = document.getElementsById("minesweeperStar");
+stars = function(){
+ this.src = "images/bulbOn.jpg"; };
+bulb.onmouseout = function(){
+ this.src = 'images/bulbOff.jpg'; };
