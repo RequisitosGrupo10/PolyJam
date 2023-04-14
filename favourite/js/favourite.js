0d3b2839dcfecbf90a46b47c1e92059787376683
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-04-14 14:24:31 +0200

    Refactor favourite.js

--- favourite/js/favourite.js
+++ favourite/js/favourite.js
@@ -2,23 +2,17 @@ const matchPairsStar = document.getElementById("matchPairsStar");
 const quickQuizStar = document.getElementById("quickQuizStar");
 const minesweeperStar = document.getElementById("minesweeperStar");
 
-matchPairsStar.addEventListener("click", function() {
-    matchPairsStar.classList.toggle("bi-star");
-    matchPairsStar.classList.toggle("bi-star-fill");
+function clicked(star) {
+    star.classList.toggle("bi-star");
+    star.classList.toggle("bi-star-fill");
 
     if (matchPairsStar.classList.contains("bi-star")) {
         console.log("Im in bi-star");
     } else {
 
     }
-});
+}
 
-quickQuizStar.addEventListener("click", function() {
-    quickQuizStar.classList.toggle("bi-star");
-    quickQuizStar.classList.toggle("bi-star-fill");
-});
-
-minesweeperStar.addEventListener("click", function() {
-    minesweeperStar.classList.toggle("bi-star");
-    minesweeperStar.classList.toggle("bi-star-fill");
-});
+matchPairsStar.addEventListener("click", () => clicked(matchPairsStar));
+quickQuizStar.addEventListener("click", () => clicked(quickQuizStar));
+minesweeperStar.addEventListener("click", () => clicked(minesweeperStar));
