3ddaad4d13b40768f87a1f70d70f946a7fc1eec8
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-04-14 14:09:22 +0200

    Modified favourite.js

--- favourite/js/favourite.js
+++ favourite/js/favourite.js
@@ -1,7 +1,18 @@
-const matchPairsStar = document.getElementsById("matchPairStar");
-const quickQuizStar = document.getElementsById("quickQuizStar");
-const minesweeperStar = document.getElementsById("minesweeperStar");
-stars = function(){
- this.src = "images/bulbOn.jpg"; };
-bulb.onmouseout = function(){
- this.src = 'images/bulbOff.jpg'; };
+const matchPairsStar = document.getElementById("matchPairsStar");
+const quickQuizStar = document.getElementById("quickQuizStar");
+const minesweeperStar = document.getElementById("minesweeperStar");
+
+matchPairsStar.addEventListener("click", function() {
+    matchPairsStar.classList.toggle("bi-star");
+    matchPairsStar.classList.toggle("bi-star-fill");
+});
+
+quickQuizStar.addEventListener("click", function() {
+    quickQuizStar.classList.toggle("bi-star");
+    quickQuizStar.classList.toggle("bi-star-fill");
+});
+
+minesweeperStar.addEventListener("click", function() {
+    minesweeperStar.classList.toggle("bi-star");
+    minesweeperStar.classList.toggle("bi-star-fill");
+});
