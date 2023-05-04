6606fc0d789e7306e7ecc669fc70ce39cf39c9d1
Javier Jordán Luque <javierjordanluque@gmail.com>
2023-05-04 12:23:01 +0200

    Renamed Index Tests

--- tests/test-index.spec.ts
+++ tests/test-index.spec.ts
@@ -109,5 +109,42 @@ test('Search action show correct status with click on search button', async ({ p
 
 test('Click on tag shows correct status', async ({ page }) => {
   const card = await page.$('div.card');
-  
+});
+
+test('clicking the empty favourite star icon toggles to a filled favourite star icon', async ({page}) => {
+  const biStarIconList = await page.$$('i.bi-star');
+  for (const biStarIcon of biStarIconList) {
+      if (biStarIcon != null) {
+          const classAttribute = await biStarIcon.getAttribute('class');
+          const isStarred = classAttribute?.includes('bi-star-fill') ?? false;
+          await biStarIcon.click();
+
+          const updatedClassAttribute = await biStarIcon.getAttribute('class');
+          const isStarredAfterClick = updatedClassAttribute?.includes('bi-star-fill') ?? false;
+      expect(isStarredAfterClick).toBe(!isStarred);
+      } else {
+          expect(false).toBe(true);
+      }
+  }
+});
+
+test('clicking the filled favourite star icon toggles to aa empty favourite star icon', async ({page}) => {
+  const biStarIconList = await page.$$('i.bi-star');
+  for (const biStarIcon of biStarIconList) {
+      await biStarIcon.click();
+  }
+  const biStarFillIconList = await page.$$('i.bi-star-fill');
+  for (const biStarFillIcon of biStarFillIconList) {
+      if (biStarFillIcon != null) {
+          const classAttribute = await biStarFillIcon.getAttribute('class');
+          const isNotStarred = !classAttribute?.includes('bi-star-fill') ?? false;
+          await biStarFillIcon.click();
+
+          const updatedClassAttribute = await biStarFillIcon.getAttribute('class');
+          const isNotStarredAfterClick = !updatedClassAttribute?.includes('bi-star-fill') ?? false;
+          expect(isNotStarredAfterClick).toBe(!isNotStarred);
+      } else {
+          expect(false).toBe(true);
+      }
+  }
 });
