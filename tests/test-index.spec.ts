20c0724c0e3709d8a72c562ffa3326620455f069
Illya Rozumovskyy <75805551+illyaro@users.noreply.github.com>
2023-05-06 19:58:40 +0200

    proper method used + index test refactoring
    to have text
    to have attribute
    is visible
    filtering locators
    index tests added

--- tests/test-index.spec.ts
+++ tests/test-index.spec.ts
@@ -1,17 +1,30 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, ElementHandle } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto("file://" + __dirname + "/../index.html");
 });
 
-test.describe('Navbar', () => {
-  test('should allow me to add todo items', async ({ page }) => {
-    await page.getByRole('link', { name: 'Favourite Games' }).click();
-    await page.getByRole('link', { name: 'Match Pairs' }).click();
-    await page.getByRole('link', { name: 'Quick Quiz' }).click();
-    await page.getByRole('link', { name: 'Minesweeper' }).click();
-    await page.getByRole('link', { name: 'Times' }).click();
-  });
+test('should allow me to add todo items', async ({ page }) => {
+  await page.getByRole('link', { name: 'Favourite Games' }).click();
+  await page.getByRole('link', { name: 'Match Pairs' }).click();
+  await page.getByRole('link', { name: 'Quick Quiz' }).click();
+  await page.getByRole('link', { name: 'Minesweeper' }).click();
+  await page.getByRole('link', { name: 'Times' }).click();
+});
+
+
+// Filtering locators feature
+test('navbar should have next links: Home, Favourite Games, Match Pairs, Quick Quiz, Minesweeper, Times', async ({ page }) => {
+  await expect (page
+    .getByRole('listitem')
+    .filter({ has: page.getByRole('link') }))
+    .toHaveText(['Home', 'Favourite Games', 'Match Pairs', 'Quick Quiz', 'Minesweeper', 'Times']);
+});
+
+// To be visible
+test('seach label is invisible', async ({ page }) => {
+  const searchLabel = await page.getByLabel('searchInput');
+  expect(await searchLabel.isVisible()).toBe(false);
 });
 
 
@@ -25,90 +38,117 @@ test('All tags has class "selectable"', async ({ page }) => {
   };
 });
 
-
-
+// To have attribute feature
 test ('All images on index has alternative text', async ({ page }) => {
-  const images = await page.$$('img');
+  const images = await page.locator('img').all();
   for (const image of images) {
-    const alt = await image.getAttribute('alt');
-    expect (alt).not.toBeNull();
-    expect(await image.getAttribute('alt')).not.toBe('');
+    const reg = RegExp('^(?!\\s*$).+')
+    await expect(image).toHaveAttribute('alt',reg);
   }  
 });
 
+// to have title feature
 test('Has title', async ({ page }) => {
   await expect(page).toHaveTitle("PolyJam");
 });
 
+// to have length feature
 test('Index has header of level 1 with content "Home"', async ({ page }) => {
-  const headers = await page.$$('h1');
-  expect(headers.length).toBe(1);
-  const header = headers[0];
-  expect(await header.innerText()).toBe('Home');
+  const headers = await page.locator('h1').all();
+  expect(headers).toHaveLength(1);
+  await expect(headers[0]).toHaveText('Home');
 });
 
+
 test('Index has header of level 2 with correct content', async ({ page }) => {
-  const headers = await page.$$('h2');
-  expect(headers.length).toBe(1);
-  const header = headers[0];
-  expect(await header.innerText()).toBe('Here you can find all of the games of our website. Enjoy!');
+  const headers = await page.locator('h2').all();
+  expect(headers).toHaveLength(1);
+  await expect(headers[0]).toHaveText('Here you can find all of the games of our website. Enjoy!');
 });
 
 test ('Each card has header of level 3', async ({ page }) => {
-  const cards = await page.$$('div.card');
+  const cards = await page.locator('div.card').all();
   for (const card of cards) {
-    const headers = await card.$$('h3');
-    expect(headers.length).toBe(1);
+    const headers = await card.locator('h3').all();
+    expect(headers).toHaveLength(1);
   }
 });
 
 test ('Index page has empty satus bar', async ({ page }) => {
-  const statusBar = await page.$('p#statusBar');
-  if(statusBar != null){
-    expect(await statusBar.innerText()).toBe('');
-  } else{
-    expect(false).toBe(true);
-  }
+  const statusBar = await page.locator('p#statusBar');
+  await expect(statusBar).toHaveText('');
 });
 
 
 test('Search action show correct status with enter key', async ({ page }) => {
-  const searchBar = await page.$('input#searchInput');
-  if(searchBar != null){
-    await searchBar.click();
-    await searchBar.fill('someSearch');
-    await searchBar.press('Enter');
-    const statusBar = await page.$('p#statusBar');
-    if(statusBar != null){
-      expect(await statusBar.innerText()).toBe('Showing results for: someSearch');
-    } else{
-      expect(false).toBe(true);
-    }
-  } else{
-    expect(false).toBe(true);
-  }
+  const searchBar = await page.getByPlaceholder("Search");
+  expect(searchBar).not.toBeNull();  
+  await searchBar.click();
+  await searchBar.fill('someSearch');
+  await searchBar.press('Enter');
+  const statusBar = await page.locator('p#statusBar');
+  await expect(statusBar).toHaveText('Showing results for: someSearch');
 });
 
 test('Search action show correct status with click on search button', async ({ page }) => {
-  const searchBar = await page.$('input#searchInput');
-  const searchButton = await page.$('button#searchButton');
-  if(searchBar != null && searchButton != null){
-    await searchBar.click();
-    await searchBar.fill('someSearch');
-    await searchButton.click();
-    const statusBar = await page.$('p#statusBar');
-    if(statusBar != null){
-      expect(await statusBar.innerText()).toBe('Showing results for: someSearch');
-    } else{
-      expect(false).toBe(true);
-    }
-  } else{
-    expect(false).toBe(true);
-  }
+  const searchBar = await page.getByPlaceholder("Search");
+  expect(searchBar).not.toBeNull();
+  const searchButton = await page.locator('button#searchButton');
+  expect(searchButton).not.toBeNull();
+  await searchBar.click();
+  await searchBar.fill('someSearch');
+  await searchButton.click();
+  const statusBar = await page.locator('p#statusBar');
+  await expect(statusBar).toHaveText('Showing results for: someSearch');
 });
 
+// to have text feature and soft assertion
 test('Click on tag shows correct status', async ({ page }) => {
-  const card = await page.$('div.card');
+  const tags = await (await page.locator('span.tag').all()).filter(async (tag) => {
+    const cl = await tag.getAttribute('class');
+    return !(cl != null ? cl.split(" ").includes("visually-hidden") : false);
+  });
+  expect(tags).not.toHaveLength(0);
+  const tag = tags[Math.floor(Math.random() * tags.length)];
+  await tag.click();
+  const statusBar = await page.locator('p#statusBar');
+  const tagText = await tag.innerText();
+  await expect(statusBar).toHaveText(tagText + " games");
+});
+
+test('Click on tag shows only games that contais that tag', async ({ page }) => {
+  const tags = await page.locator('span.tag').all();
+  if(tags.length > 0){
+    let found = false;
+    let tag;
+    while(!found){
+      tag = tags[Math.floor(Math.random() * tags.length)];
+      const cl = await tag.getAttribute('class');
+      if(tag != null && !(cl.split(" ").includes("visually-hidden"))){
+        found = true;
+      }
+    }
+    let tagText = await tag.innerText();
+    await tag.click();
+    const cards = await page.locator('div.card').all();
+    for (const card of cards) {
+      const style = await card.getAttribute('style');
+      const allTags = await card.locator('span.tag').all();
+      if(style != null && style.includes("display: none !important")){
+        for(const atag of allTags){
+          expect(await atag.innerText()).not.toBe(tagText);
+        }
+      } else{
+          let found = false;
+          for(const atag of allTags){
+            if(await atag.innerText() == tagText){
+              found = true;
+            }
+          }
+        expect(found).toBe(true);
+      }
+    }
+  }
 });
 
 test.describe('Favourite', () => {
